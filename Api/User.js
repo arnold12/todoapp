ed09a7e4b03d0e62a368ec0ddfc0aad331281602
Arnold <Arnold@sl-sp-lap-0316.local>
2020-07-02 16:17:56 +0530

    add logging

--- Api/User.js
+++ Api/User.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require('../DB/User');
+const logger = require('../utility/helper/common-functions');
 const route = express.Router();
 
 route.post('/', async (req, res)=>{
@@ -11,16 +12,19 @@ route.post('/', async (req, res)=>{
   };
   let userModel = new User(user);
   await userModel.save();
+  logger.info('User Create : ', userModel);
   res.json(userModel);
 });
 
 route.get('/', async (req, res) => {
   let userRes = await User.find({});
+  logger.info('User fetch : ', userRes);
   res.json(userRes);
 });
 
 route.put('/', async (req, res) => {
   let userRes = await User.updateMany({firstName: req.body.firstName}, {firstName: req.body.updatedFirstName});
+  logger.info('User update : ', userRes);
   res.json(userRes);
 });
 
--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const connectDB = require('./DB/Connection');
+const logger = require('./utility/helper/common-functions');
 
 const app = express();
 
 connectDB();
 app.use(express.json({extended:false}));
+app.use((req, res, next)=>{
+  const { method, url, body } = req;
+  const loginfo = {
+    timestamp: Date.now(),
+    method,
+    url,
+    body
+  };
+  logger.info('Request : ', loginfo);
+  next();
+});
 app.use('/api/userModel', require('./Api/User'));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log('server started on port :', PORT);
 });
